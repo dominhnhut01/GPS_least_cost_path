d2a68062b461da5b5d308684a1ddef62cbf2755c
Tom Ho <100968993+TungHo0206@users.noreply.github.com>
2022-05-03 16:14:19 -0400

    Delete PriorityQueueInterface.java

--- src/PriorityQueueInterface.java
+++
@@ -1,9 +0,0 @@
-public interface PriorityQueueInterface<T extends Comparable<? super T>> {
-      boolean isEmpty();
-      boolean isFull();
-      void clear();
-      int size();
-      void add(T newEntry);
-      T peek();      // returns null if empty
-      T remove();    // throws NoSuchElementException if empty
-}
