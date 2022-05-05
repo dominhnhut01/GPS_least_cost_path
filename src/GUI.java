54bcf43f65e1c5014e6c13c269d492ef9088538c
Nhut Minh Do <62295931+dominhnhut01@users.noreply.github.com>
2022-05-04 23:46:30 -0400

    Add GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -2,18 +2,24 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JCheckBox;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.imageio.ImageIO;
+
+import java.awt.event.*;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -24,53 +30,63 @@ import java.util.Scanner;
 
 public class GUI extends JFrame {
 	private static final int FRAME_WIDTH = 650;
-	private static final int FRAME_HEIGHT = 550;
+	private static final int FRAME_HEIGHT = 750;
+
+	private boolean useDist = false;
 
 	//Initialize the frame and panel
 	private JFrame frame;
 	private JPanel panelInfo;
-	private JPanel panelButton;
 	private JPanel panelContent;
+	private JPanel panelButton;
+	private JPanel picPanel;
+
+	private BufferedImage image;
 
 	//Initialize the component
 	private JButton submitButton;
 	private JLabel startLabel;
 	private JLabel endLabel;
+	private JLabel useDistLabel;
+	private JCheckBox useDistCheckbox;
 	private JTextField start;
 	private JTextField end;
+	private GraphPic graphPic;
+	private JButton buttonspec;
 
 	/** Default constructor, initialize the GUI.
 	 * no param
 	 * no return
 	 */
-	public GUI () {
+	public GUI () throws IOException {
 		setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		setTitle("Address Book Application");
+		setTitle("Finding shortest path:");
 		setLayout(new BorderLayout());
 		panelInfo = new JPanel();
-		panelInfo.setLayout(new GridLayout(2,2));
+		panelInfo.setLayout(new GridLayout(3,2));
 		TitledBorder border1 = new TitledBorder("Enter New Contact Information");
 		panelInfo.setBorder(border1);
 
 		panelButton = new JPanel();
 		panelButton.setLayout(new FlowLayout());
 
-		panelContent = new JPanel();
-		TitledBorder border3 = new TitledBorder("Saved Contacts");
-		panelContent.setBorder(border3);
-
-		//Initialize add button
 		submitButton = new JButton("Submit");
 
-		//Initialize the name field
 		start = new JTextField(20);
 		startLabel = new JLabel("Start point:     ");
 
-		//Initialize the email field
 		end = new JTextField(20);
 		endLabel = new JLabel("End point:      ");
 
-		//Initialize the TextArea, print the content already in the file "contents.txt"
+		useDistLabel = new JLabel("Use Distance:      ");
+		useDistCheckbox = new JCheckBox("");
+		useDistCheckbox.addItemListener(new CheckBoxListener());
+
+		// BufferedImage myPicture = ImageIO.read(new File("FinalProjectGraph_Basic_400x400.png"));
+		// graphPic = new JLabel(new ImageIcon(myPicture));
+
+		image = ImageIO.read(new File("FinalProjectGraph_Basic_400x400.png"));
+		graphPic = new GraphPic();
 
 		//Add all element to the panel
 		panelInfo.add(startLabel);
@@ -78,18 +94,41 @@ public class GUI extends JFrame {
 
 		panelInfo.add(endLabel);
 		panelInfo.add(end);
+		panelInfo.add(useDistLabel);
+		panelInfo.add(useDistCheckbox);
 
 		panelButton.add(submitButton);
 
+		panelContent = new JPanel();
+		panelContent.add(graphPic);
+
 		//Add panel to the frame
 		add(panelInfo, BorderLayout.NORTH);
-		add(panelContent, BorderLayout.SOUTH);
 		add(panelButton, BorderLayout.CENTER);
-
-
+		add(panelContent, BorderLayout.SOUTH);
 	}
 
-  public static void main(String[] args) {
+	class CheckBoxListener implements ItemListener {
+		public void itemStateChanged(ItemEvent event) {
+			if (event.getStateChange() == 1)
+				useDist = true;
+			else
+				useDist = false;
+			//graph.setReturnAddress(useDist)
+		}
+	}
+
+	class GraphPic extends JComponent {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 50, 50, this);
+				image.setVisible(true); // see javadoc for more info on the parameters
+    }
+	}
+
+
+  public static void main(String[] args) throws IOException {
     GUI gui = new GUI();
     gui.setResizable(false);
     gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/Graph.java
+++ src/Graph.java
@@ -10,24 +10,24 @@ public class Graph {
 	private boolean useDistCost;
 	private boolean returnAddress;
 	private HashMap <Vertex, Set<Edge>> graphData = new HashMap<Vertex, Set<Edges>>();
-	private HashMap<String, Vertex> vertices = new HashMap<String, Vertex>(); 
-	
+	private HashMap<String, Vertex> vertices = new HashMap<String, Vertex>();
+
 	public Graph() {
 		useDistCost = false;
 		returnAddress = false;
 		graphData = null;
 		vertices = null;
 	}
-	
+
 	public Graph(String fileName) {
 		this();
 		Scanner in = null;
 		try {
 			in = new Scanner(new File(fileName));
 			while (in.hasNextLine() && !in.nextLine().trim().equals("<Nodes>")) {
-				in.nextLine(); 
+				in.nextLine();
 			}
-			in.nextLine(); 
+			in.nextLine();
 			in.nextLine();
 			while (in.hasNextLine()) {
 				String[] inputVertices = in.nextLine().trim().split("\t");
@@ -42,20 +42,20 @@ public class Graph {
 					vertices.put(key, vertexToAdd);
 				}
 			}
-			
+
 			Set<Edge> edges = new TreeSet<Edge>();
-			
+
 			while (in.hasNextLine() && !in.nextLine().trim().equals("<Edges>")) {
-				in.nextLine(); 
+				in.nextLine();
 			}
-			
-			in.nextLine(); 
+
 			in.nextLine();
-			
+			in.nextLine();
+
 			String keyCheck = "";
 			String[] firstKey = in.nextLine().trim().split("\t");
 	        if (firstKey[0].equals("</Edges>")) return;
-	        
+
 	        Vertex start = vertices.get(firstKey[0]);
 	        keyCheck = start.getName();
             Vertex end = vertices.get(firstKey[1]);
@@ -63,19 +63,19 @@ public class Graph {
             int distCost = Integer.parseInt(firstKey[3]);
             Edge edgeToAdd = new Edge(start, end, timeCost, distCost);
             edges.add(edgeToAdd);
-            
+
 			while (in.hasNextLine()) {
 	            String inputEdges[] = in.nextLine().trim().split("\t");
 	            if (inputEdges[0].equals("</Edges>")) {
 	            	graphData.put(vertices.get(keyCheck), edges);
 	            	break;
 	            }
-	            
+
 	            Vertex start = vertices.get(inputEdges[0]);
 	            Vertex end = vertices.get(inputEdges[1]);
 	            int timeCost = Integer.parseInt(inputEdges[2]);
 	            int distCost = Integer.parseInt(inputEdges[3]);
-	            
+
 	            if (!start.getName().equals(keyCheck)) {
 				    graphData.put(vertices.get(keyCheck), edges);
 	                edges = new TreeSet<Edge>();
@@ -85,12 +85,12 @@ public class Graph {
 	            edgeToAdd = new Edge(start, end, timeCost, distCost);
 	            edges.add(edgeToAdd);
 			}
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Getter for useDistCost
 	 * @return boolean of useDistCost
@@ -115,7 +115,7 @@ public class Graph {
     	public void setUseDistCost(boolean useDistCost) {
         	this.useDistCost = useDistCost;
     	}
-    
+
 	/**
 	* Setter for returnAddress
 	* @param returnAddress boolean indicator
@@ -123,6 +123,4 @@ public class Graph {
     	public void setReturnAddress(boolean returnAddress) {
         	this.returnAddress = returnAddress;
     	}
-    
-
 }
