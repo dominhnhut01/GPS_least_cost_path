76e047623a12bf80f5f6b2cc3c5e1357f0e0e62b
Nhut Minh Do <62295931+dominhnhut01@users.noreply.github.com>
2022-05-05 02:23:22 -0400

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -4,131 +4,133 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
+import java.util.ArrayList;
 
 public class Graph {
 	private boolean useDistCost;
 	private boolean returnAddress;
-	private HashMap <Vertex, Set<Edge>> graphData = new HashMap<Vertex, Set<Edges>>();
-	private HashMap<String, Vertex> vertices = new HashMap<String, Vertex>(); 
-	
+	private HashMap <Vertex, ArrayList<Edge>> graphData;
+	private HashMap <String, Vertex> vertices;
+
 	public Graph() {
 		useDistCost = false;
 		returnAddress = false;
-		graphData = null;
-		vertices = null;
+		graphData = new HashMap<Vertex, ArrayList<Edge>>();
+		vertices = new HashMap<String, Vertex>();
 	}
-	
+
 	public Graph(String fileName) {
 		this();
 		Scanner in = null;
+		Vertex vertexToAdd, start, end;
+		Edge edgeToAdd;
+		int distCost, timeCost;
+
 		try {
 			in = new Scanner(new File(fileName));
+			String line;
 			while (in.hasNextLine() && !in.nextLine().trim().equals("<Nodes>")) {
-				in.nextLine(); 
+				in.nextLine();
 			}
-			in.nextLine(); 
-			in.nextLine();
+			line = in.nextLine();
+
+			String key, address;
+			int x, y;
 			while (in.hasNextLine()) {
-				String[] inputVertices = in.nextLine().trim().split("\t");
+				line = in.nextLine();
+				String[] inputVertices = line.trim().split("\t");
 				if (inputVertices[0].equals("</Nodes>")) {
 					break;
 				} else {
-					String key = inputVertices[0];
-					String address = inputVertices[1];
-					int x = Integer.parseInt(inputVertices[2]);
-					int y = Integer.parseInt(inputVertices[3]);
-					Vertex vertexToAdd = new Vertex(key, address, x, y);
+					key = inputVertices[0];
+					address = inputVertices[1];
+					x = Integer.parseInt(inputVertices[2]);
+					y = Integer.parseInt(inputVertices[3]);
+					vertexToAdd = new Vertex(key, address, x, y);
 					vertices.put(key, vertexToAdd);
 				}
 			}
-			
-			Set<Edge> edges = new TreeSet<Edge>();
-			
-			while (in.hasNextLine() && !in.nextLine().trim().equals("<Edges>")) {
-				in.nextLine(); 
-			}
-			
-			in.nextLine(); 
-			in.nextLine();
-			
-			String keyCheck = "";
-			String[] firstKey = in.nextLine().trim().split("\t");
-	        if (firstKey[0].equals("</Edges>")) return;
-	        
-	        Vertex start = vertices.get(firstKey[0]);
-	        keyCheck = start.getName();
-            Vertex end = vertices.get(firstKey[1]);
-            int timeCost = Integer.parseInt(firstKey[2]);
-            int distCost = Integer.parseInt(firstKey[3]);
-            Edge edgeToAdd = new Edge(start, end, timeCost, distCost);
-            edges.add(edgeToAdd);
-            
-			while (in.hasNextLine()) {
-	            String inputEdges[] = in.nextLine().trim().split("\t");
-	            if (inputEdges[0].equals("</Edges>")) {
-	            	graphData.put(vertices.get(keyCheck), edges);
-	            	break;
-	            }
-	            
-	            Vertex start = vertices.get(inputEdges[0]);
-	            Vertex end = vertices.get(inputEdges[1]);
-	            int timeCost = Integer.parseInt(inputEdges[2]);
-	            int distCost = Integer.parseInt(inputEdges[3]);
-	            
-	            if (!start.getName().equals(keyCheck)) {
-				    graphData.put(vertices.get(keyCheck), edges);
-	                edges = new TreeSet<Edge>();
-	                keyCheck = start.getName();
-	            }
 
-	            edgeToAdd = new Edge(start, end, timeCost, distCost);
-	            edges.add(edgeToAdd);
+			ArrayList<Edge> edges = new ArrayList<Edge>();
+			System.out.println("--------------------------");
+
+			// Skips lines until Edges are reached
+      while (!line.equals("<Edges>")) { line = in.nextLine(); }
+      in.nextLine();
+
+			String keyCheck = "";
+			line = in.nextLine();
+			String[] firstKey = line.trim().split("\t");
+
+      start = vertices.get(firstKey[0]);
+
+      keyCheck = start.getName();
+
+      end = vertices.get(firstKey[1]);
+      timeCost = Integer.parseInt(firstKey[2]);
+      distCost = Integer.parseInt(firstKey[3]);
+      edgeToAdd = new Edge(start, end, timeCost, distCost);
+      edges.add(edgeToAdd);
+
+			while (in.hasNextLine()) {
+				line = in.nextLine();
+	      String inputEdges[] = line.trim().split("\t");
+	      if (inputEdges[0].equals("</Edges>")) {
+	      	graphData.put(vertices.get(keyCheck), edges);
+	      	break;
+	      }
+
+	      start = vertices.get(inputEdges[0]);
+	      end = vertices.get(inputEdges[1]);
+	      timeCost = Integer.parseInt(inputEdges[2]);
+	      distCost = Integer.parseInt(inputEdges[3]);
+
+	      edgeToAdd = new Edge(start, end, timeCost, distCost);
+	      edges.add(edgeToAdd);
 			}
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public findShortestPath(String start, String end) {
+		Vertex startVertex = vertices.get(start);
+		Vertex endVertex = vertices.get(end);
+
+	}
+
+
 	/**
 	 * Getter for useDistCost
 	 * @return boolean of useDistCost
 	 */
 	public boolean getUseDistCost() {
-        return useDistCost;
-    }
+  	return useDistCost;
+  }
 
 	/**
 	 * Getter for returnAddress
 	 * @return boolean of returnAddress
 	 */
-    public boolean getReturnAddress() {
-        return returnAddress;
-    }
+   	 public boolean getReturnAddress() {
+      	return returnAddress;
+   	 }
 
 
 	/**
 	 * Setter for useDistCost
 	 * @param useDistCost boolean indicator
 	 */
-    public void setUseDistCost(boolean useDistCost) {
-        this.useDistCost = useDistCost;
-    }
-    
-    /**
-     * Setter for returnAddress
-     * @param returnAddress boolean indicator
-     */
-    public void setReturnAddress(boolean returnAddress) {
-        this.returnAddress = returnAddress;
-    }
-    
-    public HashMap<String, Vertex> getVertices (){
-    	return vertices;
-    }
-    
-    public HashMap <Vertex, Set<Edge>> getGraphData (){
-    	return graphData;
-    }
-}
+    	public void setUseDistCost(boolean useDistCost) {
+      	this.useDistCost = useDistCost;
+    	}
+
+	/**
+	* Setter for returnAddress
+	* @param returnAddress boolean indicator
+	*/
+    	public void setReturnAddress(boolean returnAddress) {
+      	this.returnAddress = returnAddress;
+    	}
+	}
